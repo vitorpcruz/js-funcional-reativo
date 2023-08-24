2d21eb306787bbdf0d39bdd10ff77a88ec936502
Vitor P Cruz <vitor-pcruz@hotmail.com>
2023-08-24 07:34:27 -0300

    separando linhas

--- sec2-projeto-1/index.js
+++ sec2-projeto-1/index.js
@@ -5,5 +5,7 @@ const caminho = path.join(__dirname, "./legendas");
 fn.lerDiretorio(caminho)
   .then((arquivos) => fn.elementosTerminadosCom(arquivos, ".srt"))
   .then(fn.lerArquivos)
+  .then((conteudos) => conteudos.join("\n"))
+  .then((todoConteudo) => todoConteudo.split("\n"))
   //.then()
   .then(console.log);
