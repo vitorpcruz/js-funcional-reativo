c50f75d24aeee2ca17cd35a094e11960a03ca778
Vitor P Cruz <vitor-pcruz@hotmail.com>
2023-08-24 07:14:20 -0300

    iniciando

---
+++ sec2-projeto-1/index.js
@@ -0,0 +1,6 @@
+const path = require("path");
+const fn = require("./funcoes");
+
+const caminho = path.join(__dirname, "./legendas");
+const arquivos = fn.lerDiretorio(caminho);
+console.log(arquivos);
